a9f604e9dd3d855145ba412f004064ed7f898d46
mario ciari <mario.ciari@yahoo.fr>
2021-06-12 16:19:00 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -1,45 +1,80 @@
 const inputs = document.querySelectorAll('input[type="text"] , input[type="email"], input[type="password"]');
-
+let countError=0;
 const displayError = (type,field, message) => {
     
     
     const fieldTarget = document.querySelector(`.container-${field}`);
     const small = fieldTarget.querySelector('small');
+    const progressBar = fieldTarget.querySelector('p.progressBar')
 
     if(type === 'error') {
       fieldTarget.classList.add('error');
       small.textContent = message;
+      progressBar.classList.add('error')
     } 
 }
 
 const removeError = (field,message="") => {
     const fieldTarget = document.querySelector(`.container-${field}`);
     const small = fieldTarget.querySelector('small');
-
+    const progressBar = fieldTarget.querySelector('p.progressBar');
     fieldTarget.classList.remove('error');
     small.textContent = message;
-
+    progressBar.classList.remove('error');
+    progressBar.classList.add('success');
 }
 
 const pseudoChecker = (value) => {
     
     if(value.length < 4 || value.length > 20) {
-        displayError('error','pseudo','Le pseudo doit avoir entre 4 et 20 caractères',)
+        displayError('error','pseudo','Le pseudo doit avoir entre 4 et 20 caractères')
+       countError++;
+        return false;
     }
     else if(value.length > 4  && value.length < 20) {
      removeError('pseudo')
+     countError=0;
+     return true;
 }
 }
 
 const emailChecker = (value) => {
     
-    if(value.length < 4 || value.length > 20) {
-        displayError('error','email',"Le format de l'email n'est pas valide",)
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if( !re.test(String(value).toLowerCase()) ) {
+        displayError('error','email',"Le format de l'email n'est pas valide")
+        countError++;
+        return false;
     }
-    else if(value.length > 4  && value.length < 20) {
-     removeError('email')
+    else {
+        removeError('email',"")
+        countError=0;
+        return true
+    }
 }
+
+const passwordChecker = (password) => {
+    
+    confirmPassword(document.querySelector('#confirmPassword').value)
+
 }
+
+const confirmPassword = (passwordConfirm) => {
+
+    const password = document.querySelector('#password').value;
+   
+    if(password != passwordConfirm ) {
+        displayError('error','confirmPassword',' Les mots de passe ne correspondent pas');
+        countError++;
+    }
+    else {
+
+        removeError('confirmPassword');
+        countError=0;
+    }
+   
+}
+
 
 inputs.forEach(input => {
     input.addEventListener('input', (e) => {
@@ -54,11 +89,11 @@ inputs.forEach(input => {
                 break;
 
             case "password":
-                passwordChecker();
+                passwordChecker(e.target.value);
                 break;
 
             case "confirmPassword":
-                console.log('vous etes sur confirmation');
+                confirmPassword(e.target.value);
                 break;
 
 
@@ -66,6 +101,18 @@ inputs.forEach(input => {
 
     })
 })
+
+const register = (e) => {
+    e.preventDefault();
+
+    console.log(countError);
+    if(countError >1) {
+        console.log('c pas bon');
+    }
+}
+
+
+myForm.addEventListener('submit',register)
 
 
 
