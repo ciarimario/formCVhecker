213b708bbc50fa699391e96e01e3f8683d0cf87d
mario ciari <mario.ciari@yahoo.fr>
2021-06-12 23:19:51 +0200

    release one

--- js/app.js
+++ js/app.js
@@ -1,40 +1,52 @@
 const inputs = document.querySelectorAll('input[type="text"] , input[type="email"], input[type="password"]');
 let countError=0;
+let verifyPseudo=false;
+let verifyEmail = false;
+let verifyPassword = false;
+let verifyConfirmPassword=false;
 const displayError = (type,field, message) => {
     
     
     const fieldTarget = document.querySelector(`.container-${field}`);
     const small = fieldTarget.querySelector('small');
     const progressBar = fieldTarget.querySelector('p.progressBar')
+    const i = fieldTarget.querySelector('i.fas');
 
     if(type === 'error') {
       fieldTarget.classList.add('error');
       small.textContent = message;
       progressBar.classList.add('error')
+      i.classList.add('error')
     } 
 }
 
-const removeError = (field,message="") => {
+const removeError = (field,message="",progressBarStrength="strong") => {
     const fieldTarget = document.querySelector(`.container-${field}`);
     const small = fieldTarget.querySelector('small');
     const progressBar = fieldTarget.querySelector('p.progressBar');
+    const i = fieldTarget.querySelector('i.fas');
     fieldTarget.classList.remove('error');
     small.textContent = message;
     progressBar.classList.remove('error');
-    progressBar.classList.add('success');
+    if(progressBarStrength == 'medium') {
+        progressBar.classList.add('success50');
+    } else if(progressBarStrength == 'strong') {
+        progressBar.classList.remove("success50")
+        progressBar.classList.add('success');
+    }
+    i.classList.remove('error');
+    i.classList.add('success');
 }
 
 const pseudoChecker = (value) => {
     
     if(value.length < 4 || value.length > 20) {
         displayError('error','pseudo','Le pseudo doit avoir entre 4 et 20 caractères')
-       countError++;
-        return false;
+       verifyPseudo = false;
     }
     else if(value.length > 4  && value.length < 20) {
      removeError('pseudo')
-     countError=0;
-     return true;
+     verifyPseudo = true;
 }
 }
 
@@ -43,17 +55,35 @@ const emailChecker = (value) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if( !re.test(String(value).toLowerCase()) ) {
         displayError('error','email',"Le format de l'email n'est pas valide")
-        countError++;
-        return false;
+        verifyEmail = false;
     }
     else {
         removeError('email',"")
-        countError=0;
-        return true
+        verifyEmail = true;
     }
 }
 
 const passwordChecker = (password) => {
+
+    const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    const mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+    if(password.length < 8) {
+        displayError('error','password','Le mot de passe doit contenir au moins 8 caractères')
+        verifyPassword=false;
+    } else {
+        if(strongRegex.test(password)) {
+            removeError('password','Sécurité password forte')
+            verifyPassword=true;
+    
+        } else if(mediumRegex.test(password)) {
+            removeError('password','Sécurité password moyenne','medium')
+            verifyPassword = true;
+        } else {
+            displayError('error','password','Le mot de passe doit contenir au moins 1 majuscule, 1 chiffre et 1 caractère spécial');
+            verifyPassword = false;
+        }
+    }
+    
     
     confirmPassword(document.querySelector('#confirmPassword').value)
 
@@ -65,12 +95,12 @@ const confirmPassword = (passwordConfirm) => {
    
     if(password != passwordConfirm ) {
         displayError('error','confirmPassword',' Les mots de passe ne correspondent pas');
-        countError++;
+        verifyConfirmPassword = false;
     }
     else {
 
         removeError('confirmPassword');
-        countError=0;
+        verifyConfirmPassword=true;
     }
    
 }
@@ -82,18 +112,22 @@ inputs.forEach(input => {
         switch(e.target.id) {
             case "pseudo":
                 pseudoChecker(e.target.value);
+                checkAllFields();
                 break;
-
-            case "email":
+                
+                case "email":
                 emailChecker(e.target.value);
+                checkAllFields();
                 break;
 
             case "password":
                 passwordChecker(e.target.value);
+                checkAllFields();
                 break;
 
             case "confirmPassword":
                 confirmPassword(e.target.value);
+                checkAllFields();
                 break;
 
 
@@ -102,13 +136,23 @@ inputs.forEach(input => {
     })
 })
 
+const checkAllFields = () => {
+
+    if(verifyPseudo && verifyEmail && verifyPassword && verifyConfirmPassword) {
+        submit.disabled = false;
+    } else {
+        submit.disabled = true;
+    }
+}
 const register = (e) => {
     e.preventDefault();
+    if(verifyPseudo && verifyEmail && verifyPassword && verifyConfirmPassword) {
+        submit.disabled = false;
+        
+    } else {
 
-    console.log(countError);
-    if(countError >1) {
-        console.log('c pas bon');
     }
+    
 }
 
 
